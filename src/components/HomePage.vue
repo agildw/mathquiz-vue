be6d71adbeba423be67dbf92728ff7bcd0dc6d5d
agildw <yudistiraagil24@gmail.com>
2021-10-03 16:01:28 +0700

    add input component and condiional rendering

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,19 +2,28 @@
   <v-container fill-height>
     <v-row align-content="center" align="center" justify="center">
       <v-col>
-        <v-card elevation="2" width="461" height="440">
+        <v-card elevation="7" width="461" height="440">
           <v-card-title>Soal</v-card-title>
           <v-card-text>
             {{ soalNya }}
+            <v-text-field
+              placeholder="Jawab"
+              v-model.lazy="inputAnswer"
+            ></v-text-field>
           </v-card-text>
+          <v-card-actions>
+            <v-btn @click="submitAnswer()">Submit</v-btn>
+          </v-card-actions>
         </v-card>
       </v-col>
       <v-col>
-        <v-card elevation="2" width="461" height="440">
+        <v-card elevation="7" width="461" height="440">
           <v-card-title>Hasil</v-card-title>
           <v-card-text>
             {{ hasilnya }}
           </v-card-text>
+          <v-card-text v-if="isCorrect == true">Benar</v-card-text>
+          <v-card-text v-else-if="isCorrect == false">salah</v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -31,6 +40,8 @@ export default {
       operatorNya: null,
       soalNya: null,
       hasilnya: null,
+      inputAnswer: null,
+      isCorrect: null,
     };
   },
   methods: {
@@ -43,6 +54,18 @@ export default {
     resultMath() {
       this.soalNya = this.firstNumber + this.operatorNya + this.secondNumber;
       this.hasilnya = eval(this.soalNya);
+    },
+    submitAnswer() {
+      console.log(this.isCorrect);
+      if (this.inputAnswer == this.hasilnya) {
+        this.isCorrect = true;
+        console.log(this.isCorrect);
+        console.log(this.inputAnswer);
+        console.log(this.hasilnya);
+      } else {
+        this.isCorrect = false;
+        // console.log(this.isCorrect);
+      }
     },
   },
   created() {
